ffaea2066a211d2227c281d51ba9a2766a46c756
TatsuakiIto <gratachu9@gmail.com>
2023-07-21 21:10:08 +0900

    useRef動作検証

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
@@ -7,12 +7,19 @@ function App() {
     {id: 2, name: "Todo2", completed: true}
   ]);
 
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    // タスクを追加する処理
+    console.log(todoNameRef);
+  };
+
   return (
     <div>
       {/* todos という名前でTodoListというコンポーネントへ渡すという記述になる */}
       <TodoList todos={todos}/>
-      <input type="text" />
-      <button>タスクの追加</button>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクの追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク数: 0</div>
     </div>
